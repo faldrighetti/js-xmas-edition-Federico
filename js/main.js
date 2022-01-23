4eb8ddd7d1ab34fd8398496077c68c2d5f703c7c
faldrighetti <56283793+FedericoA13@users.noreply.github.com>
2022-01-23 00:33:06 -0300

    Mejora usando nuevas técnicas
    Integra forEach, RegEx, objetos

--- js/main.js
+++ js/main.js
@@ -44,37 +44,59 @@ function validarFormulario(event){
     const errores = {
         nombre: errorNombre,
         ciudad: errorCiudad,
-        descripcionRegalo: errorDescripcionRegalo
+        'descripcion-regalo': errorDescripcionRegalo
     }
 
-    manejarErrores(errores)
+    const esExito = manejarErrores(errores) === 0
+
+    if(esExito){
+        $form.className = 'oculto'
+        document.querySelector('#exito').className = ''
+    }
 
     event.preventDefault()
 }
 
 function manejarErrores(errores){
-    errorNombre = errores.nombre
-    errorCiudad = errores.ciudad
-    errorDescripcionRegalo = errores.descripcionRegalo
 
-    if(errorNombre){
-        $form.nombre.className = 'error'
-    } else {
-        $form.nombre.className = ''
-    }
+    const llaves = Object.keys(errores)
+    const $errores = document.querySelector('#errores')
 
-    if(errorCiudad){
-        $form.ciudad.className = 'error'
-    } else {
-        $form.ciudad.className = ''
-    }
+    let cantidadErrores = 0
 
-    if(errorDescripcionRegalo){
-        $form['descripcion-regalo'].className = 'error'
-    } else {
-        $form['descripcion-regalo'].className = ''
-    }
+    llaves.forEach(function(llave){
+        const error = (errores[llave])
+
+        if(error){
+            cantidadErrores ++;
+            $form[llave].className = 'error'
+
+            const $error = document.createElement('li');
+            $error.innerText = error
+            $errores.appendChild($error)
+
+        } else {
+            // TAREA: EN ESTE ELSE BORRAR EL CAMPO ADECUADO
+            $form[llave].className = ''
+        }
+    })
+
+    return cantidadErrores
 }
 
 const $form = document.querySelector('#carta-a-santa')
 $form.onsubmit = validarFormulario;
+
+/*
+
+Tarea clase 8: 
+1- A las tareas de la clase 6, hacer las validaciones y pruebas necesarias. Usar RegEx, objetos, forEach, poner estilos,
+    mostrar los errores en la interfaz de usuario y hacer lo mismo con el éxito.
+    Escribir pruebas. 
+    Las edades no pueden tener decimales.
+2- Arreglar bug del formulario, que cuando se envía la carta con algún error por segunda vez, se vuelven a imprimir
+    los errores
+3- Hacer el último punto del nivel 1, donde el mensaje de éxito se muestra 5 segundos y se redirecciona a wishlist.html
+
+
+*/
